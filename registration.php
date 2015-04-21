3a406484cd7b58259a2d0ac9ed1d73bde0fde04a
hercegtomas <tomastomherceg@gmail.com>
2015-04-21 20:09:56 +0200

    Update registration
    updated registration form

--- registration.php
+++ registration.php
@@ -4,14 +4,21 @@ Include('connect.php');
 //if submit is not blanked i.e. it is clicked.
 If(isset($_REQUEST['submit'])!='')
 {
-If($_REQUEST['name']=='' || $_REQUEST['email']=='' || $_REQUEST['password']==''|| $_REQUEST['repassword']=='')
+If($_REQUEST['fname']=='' || $_REQUEST['lname']=='' || $_REQUEST['street']=='' || $_REQUEST['zip']=='' 
+	|| $_REQUEST['city']=='' || $_REQUEST['country']=='' || $_REQUEST['tel']=='' || $_REQUEST['email']=='')
+
+
+	// || $_REQUEST['password']==''|| $_REQUEST['repassword']=='')
 {
 Echo "please fill the empty field.";
 }
 Else
 {
-$sql="insert into hotel(name,email,password,repassword) values('".$_REQUEST['name']."', '".$_REQUEST['email']."', 
-	'".$_REQUEST['password']."', '".$_REQUEST['repassword']."')";
+$sql="insert into hotel into guest (Name,Surename,Adress,Country,Zip,City,Mobile,Email)
+    values('".$_REQUEST['fname']."', '".$_REQUEST['lname']."', '".$_REQUEST['street']."', '".$_REQUEST['zip']."', 
+    	'".$_REQUEST['city']."', '".$_REQUEST['country']."', '".$_REQUEST['tel']."', '".$_REQUEST['email']."')"; 
+	
+	// '".$_REQUEST['password']."', '".$_REQUEST['repassword']."')"; 
 $res=mysql_query($sql);
 If($res)
 {
